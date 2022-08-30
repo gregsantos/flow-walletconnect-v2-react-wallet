66e009864f3f56da5a92f3f8f5fa0d93a126f43a
gregsantos <greg.santos@dapperlabs.com>
2022-08-30 16:07:26 -0700

    update: change metadata url

--- src/utils/WalletConnectUtil.ts
+++ src/utils/WalletConnectUtil.ts
@@ -7,9 +7,9 @@ export async function createSignClient() {
     projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
     relayUrl: process.env.NEXT_PUBLIC_RELAY_URL ?? 'wss://relay.walletconnect.com',
     metadata: {
-      name: 'React Wallet',
-      description: 'React Wallet for WalletConnect',
-      url: 'https://walletconnect.com/',
+      name: 'FCL/WC React Wallet',
+      description: 'React Wallet for FCL WalletConnect',
+      url: 'https://flow-walletconnect-v2-react-wallet.vercel.app',
       icons: ['https://avatars.githubusercontent.com/u/37784886']
     }
   })
